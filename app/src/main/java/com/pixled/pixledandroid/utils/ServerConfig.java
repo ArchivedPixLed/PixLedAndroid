4a8eee40121f8ace96d7cc6da813c2fc025a331b
Paul Breugnot <paul.breugnot@etu.emse.fr>
2019-03-09 16:27:50 +0100

    Manual IP config

--- app/src/main/java/com/pixled/pixledandroid/utils/ServerConfig.java
+++ app/src/main/java/com/pixled/pixledandroid/utils/ServerConfig.java
@@ -16,13 +16,13 @@ public abstract class ServerConfig {
     // public static final String URI = "tcp://192.168.1.124:1883";
     public static String URI;
 
-    public static void setEndpoint(int serverPort, InetAddress serverHost) {
-        ENDPOINT = "http://" + serverHost.getCanonicalHostName() + ":" + String.valueOf(serverPort) + "/api/";
+    public static void setEndpoint(int serverPort, String serverHost) {
+        ENDPOINT = "http://" + serverHost + ":" + String.valueOf(serverPort) + "/api/";
         Log.i(TAG, "Server endpoint : " + ENDPOINT);
     }
 
-    public static void setBrokerUri(int brokerPort, InetAddress brokerHost) {
-        URI = "tcp://" + brokerHost.getCanonicalHostName() + ":" + String.valueOf(brokerPort);
+    public static void setBrokerUri(int brokerPort, String brokerHost) {
+        URI = "tcp://" + brokerHost + ":" + String.valueOf(brokerPort);
         Log.i(TAG, "Broker uri : " + URI);
     }
 }
--- app/src/main/java/com/pixled/pixledandroid/welcome/WelcomeActivity.java
+++ app/src/main/java/com/pixled/pixledandroid/welcome/WelcomeActivity.java
@@ -1,14 +1,24 @@
 package com.pixled.pixledandroid.welcome;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.nsd.NsdManager;
 import android.net.nsd.NsdServiceInfo;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.pixled.pixledandroid.R;
@@ -23,10 +33,15 @@ import java.net.InetAddress;
 public class WelcomeActivity extends AppCompatActivity {
 
     private static final String TAG = "LOADING";
+    private static final String PREFERENCES = "server_config.xml";
+
+    private SharedPreferences preferences;
 
     public static MqttAndroidConnection mqttAndroidConnection;
 
     private Button startButton;
+
+    private Toolbar toolbar;
 
     private NsdManager mNsdManager;
     private NsdManager.DiscoveryListener discoveryListener;
@@ -39,7 +54,16 @@ public class WelcomeActivity extends AppCompatActivity {
     private int brokerPort;
     private InetAddress brokerHost;
 
+    private boolean forceDiscoveryStop = false;
+
     private boolean mqttConnectionOk = false;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.welcome_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,19 +71,93 @@ public class WelcomeActivity extends AppCompatActivity {
 
         setContentView(R.layout.welcome);
 
+        // Set up tool bar
+        toolbar = findViewById(R.id.welcomeToolbar);
+        setSupportActionBar(toolbar);
 
-        mNsdManager =  (NsdManager) getApplicationContext().getSystemService(Context.NSD_SERVICE);
+        toolbar.inflateMenu(R.menu.welcome_menu);
+
+        preferences = this.getSharedPreferences(PREFERENCES, 0);
+
+        String server_ip = preferences.getString("server_ip", null);
+        if (server_ip != null && server_ip.length() > 0) {
+            Log.i(TAG, "Manually set up server ip found : " + server_ip);
+            ServerConfig.setEndpoint(8080, server_ip);
+            ServerConfig.setBrokerUri(1883, server_ip);
+            initMqttConnection();
+        }
+        else {
+            mNsdManager =  (NsdManager) getApplicationContext().getSystemService(Context.NSD_SERVICE);
+            discoverServices();
+        }
+
+
 
         startButton = findViewById(R.id.welcomeButton);
-        startButton.setEnabled(false);
-        startButton.getBackground().setAlpha(100);
+
         startButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 launchGroupSelectionActivity();
             }
         });
 
-        discoverServices();
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.set_server_ip:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                // Get the layout inflater
+                LayoutInflater inflater = this.getLayoutInflater();
+
+                final View saveIpDialog = inflater.inflate(R.layout.save_ip_dialog, null);
+                EditText ip_input = saveIpDialog.findViewById(R.id.input_server_ip);
+
+                String server_ip = preferences.getString("server_ip", null);
+                if (server_ip != null) {
+                    ip_input.setText(server_ip);
+                }
+                // Inflate and set the layout for the dialog
+                // Pass null as the parent view because its going in the dialog layout
+                builder.setView(saveIpDialog)
+                        // Add action buttons
+                        .setPositiveButton(R.string.save_ip, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                // sign in the user ...
+
+                                String server_ip = ip_input.getText().toString();
+                                preferences.edit().putString("server_ip", server_ip).apply();
+                                if (server_ip.length() > 0) {
+                                    forceDiscoveryStop = true;
+                                    if (mNsdManager != null) {
+                                        // The discovery service had been started
+                                        mNsdManager.stopServiceDiscovery(discoveryListener);
+                                    }
+                                    ServerConfig.setEndpoint(8080, server_ip);
+                                    ServerConfig.setBrokerUri(1883, server_ip);
+                                    initMqttConnection();
+                                }
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        })
+                        .show();
+
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     protected void launchGroupSelectionActivity(){
@@ -87,7 +185,7 @@ public class WelcomeActivity extends AppCompatActivity {
                     serverHost = mService.getHost();
                     serverOk = true;
 
-                    ServerConfig.setEndpoint(serverPort, serverHost);
+                    ServerConfig.setEndpoint(serverPort, serverHost.getCanonicalHostName());
 
                     updateServerTaskStatus();
                     mNsdManager.stopServiceDiscovery(discoveryListener);
@@ -116,7 +214,7 @@ public class WelcomeActivity extends AppCompatActivity {
                     brokerHost = mService.getHost();
                     brokerOk = true;
 
-                    ServerConfig.setBrokerUri(brokerPort, brokerHost);
+                    ServerConfig.setBrokerUri(brokerPort, brokerHost.getCanonicalHostName());
 
                     updateBrokerTaskStatus();
                     mNsdManager.stopServiceDiscovery(discoveryListener);
@@ -178,12 +276,14 @@ public class WelcomeActivity extends AppCompatActivity {
             @Override
             public void onDiscoveryStopped(String serviceType) {
                 Log.i(TAG, "Discovery stopped: " + serviceType);
-                if (!brokerOk) {
-                    mNsdManager.discoverServices(
-                            "_mqtt._tcp.", NsdManager.PROTOCOL_DNS_SD, discoveryListener);
-                }
-                else {
-                    initMqttConnection();
+                if(!forceDiscoveryStop) {
+                    if (!brokerOk) {
+                        mNsdManager.discoverServices(
+                                "_mqtt._tcp.", NsdManager.PROTOCOL_DNS_SD, discoveryListener);
+                    }
+                    else {
+                        initMqttConnection();
+                    }
                 }
             }
 
@@ -234,8 +334,7 @@ public class WelcomeActivity extends AppCompatActivity {
                 TextView checkMqttConnection = findViewById(R.id.taskStatus);
                 findViewById(R.id.taskView).setVisibility(View.GONE);
 
-                findViewById(R.id.welcomeButton).setEnabled(true);
-                startButton.getBackground().setAlpha(255);
+                startButton.setVisibility(View.VISIBLE);
                 launchGroupSelectionActivity();
             }
         });
